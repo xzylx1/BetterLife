b677e49934a012eb7f52ce8c296b9a7e13d4e64b
xzylx1 <xzylx1@qq.com>
2016-02-26 16:08:34 +0800

    惠生活 享生活！ 2.26

--- app/src/main/java/com/nava_b/betterlife/ViewPagerAdapter.java
+++ app/src/main/java/com/nava_b/betterlife/ViewPagerAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.Random;
 
 public class ViewPagerAdapter extends PagerAdapter
@@ -16,27 +17,33 @@ public class ViewPagerAdapter extends PagerAdapter
     private final Random random = new Random();
     private final SparseArray<TextView> mHolderArray = new SparseArray<>();
     private int mSize;
+    private List imageList;
 
     public ViewPagerAdapter() {
         mSize = 5;
     }
 
-    public ViewPagerAdapter(int count) {
-        mSize = count;
+    public ViewPagerAdapter(List imgList) {
+        mSize = imgList.size();
+
     }
 
+    // 获取要滑动的控件的数量，在这里我们以滑动的广告栏为例，那么这里就应该是展示的广告图片的ImageView数量
     @Override public int getCount() {
         return mSize;
     }
 
+    // 来判断显示的是否是同一张图片，这里我们将两个参数相比较返回即可
     @Override public boolean isViewFromObject(View view, Object object) {
         return view == object;
     }
 
+    // PagerAdapter只缓存三张要显示的图片，如果滑动的图片超出了缓存的范围，就会调用这个方法，将图片销毁
     @Override public void destroyItem(ViewGroup view, int position, Object object) {
         view.removeView(mHolderArray.get(position));
     }
 
+    // 当要显示的图片可以进行缓存的时候，会调用这个方法进行显示图片的初始化，我们将要显示的ImageView加入到ViewGroup中，然后作为返回值返回即可
     @Override public Object instantiateItem(ViewGroup view, int position) {
         TextView textView = new TextView(view.getContext());
         textView.setText(String.valueOf(position + 1));
--- app/src/main/java/com/nava_b/betterlife/fragment/Home_fragment.java
+++ app/src/main/java/com/nava_b/betterlife/fragment/Home_fragment.java
@@ -1,33 +1,55 @@
 package com.nava_b.betterlife.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.nava_b.betterlife.R;
+
+import java.util.Random;
 
 
 public class Home_fragment extends Fragment
 {
 
-    public static Fragment newInstance() {
+    @Bind(R.id.home_page_listview)
+    ListView homePageListview;
+    private Random random = new Random();
+
+    public static Fragment newInstance()
+    {
         Fragment fragment = new Home_fragment();
         return fragment;
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_home_layout, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
-    public void onActivityCreated(Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
         super.onActivityCreated(savedInstanceState);
 
     }
 
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         super.onPause();
     }
 
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
